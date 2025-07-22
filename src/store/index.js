97cbfc8d1cf3a58e188f24d9827ecb38893320dc
vany <ellensem@student.21-school.ru>
2025-07-22 11:19:03 +0300

    test

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import App from './App';
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './cartSlice';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
